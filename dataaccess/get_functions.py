2308e8a2c69bfcdbe5fbf4bba22f26a9b37c6643
rohanmohapatra <rohan.ron14@gmail.com>
2019-11-16 22:11:11 +0530

    Completed Patient View

--- dataaccess/get_functions.py
+++ dataaccess/get_functions.py
@@ -12,10 +12,15 @@ def get_patient_metadata(patient_id):
 
 def get_patient_vaccination_data(patient_id):
 	vaccinations_collection = mongo.db.vaccinations
-	result = vaccinations_collection.find_one({patient_id})
+	result = vaccinations_collection.find_one({"_id":patient_id})
 	return result
 
 def get_organization(organization_id):
 	organizations_collection = mongo.db.organizations 
 	result = organizations_collection.find_one({"organization_id":organization_id})
+	return result
+
+def get_patient_metadata_with_username(username):
+	patients_collection = mongo.db.patients
+	result = patients_collection.find_one({"username" : username})
 	return result
--- views/patient_view.py
+++ views/patient_view.py
@@ -1,7 +1,35 @@
-from flask import Blueprint
+from flask import Blueprint,Response, jsonify, request
+from dataaccess.get_functions import *
+import json
 
 patient_view = Blueprint('patient_view',__name__)
 
-@patient_view.route("/get_personal_details", methods=('GET'))
+@patient_view.route("/")
+def test_patient():
+    print("Working")
+    return Response(status=200)
+
+@patient_view.route("/get_personal_details/<patient_id>", methods=['GET'])
+def get_personal_details_with_id(patient_id):
+    print("View Reached with id{}".format(patient_id))
+    data = get_patient_metadata(patient_id)
+    print(type(data))
+    data.pop("username",None)
+    data.pop("password",None)
+    return jsonify(data)
+
+@patient_view.route("/get_personal_details/", methods=['GET'])
 def get_personal_details():
-    
+    print("View Reached with ")
+    return Response(status=200)
+
+@patient_view.route("/get_patient_id/",methods=["POST"])
+def get_patient_id():
+    json_data = request.get_json(force=True)
+    data = get_patient_metadata_with_username(json_data["username"])
+    return jsonify({"patient_id":data["patient_id"]})
+
+@patient_view.route("/get_patient_vaccination_details/<patient_id>", methods=["GET"])
+def get_patient_vaccination_details_controller(patient_id):
+    data = get_patient_vaccination_data(patient_id)
+    return jsonify(data[patient_id])
