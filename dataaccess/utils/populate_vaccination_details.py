d763c7d1145dd10b855a6e65d23725581ae0c297
rohanmohapatra <rohan.ron14@gmail.com>
2019-11-16 20:15:20 +0530

    Vaccination Detials added Successfully

--- dataaccess/utils/populate_vaccination_details.py
+++ dataaccess/utils/populate_vaccination_details.py
@@ -5,10 +5,29 @@ currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 import get_functions
+import post_functions
+
 def populate_vaccination_details():
     vaccination_details = 0
     with open('vaccination.json') as f:
         vaccination_details = json.loads(f.read())
-    print(vaccination_details)
+
+    for eachPatient in vaccination_details:
+        for eachVaccine in eachPatient:
+            for i in range(len(eachPatient[eachVaccine])):
+                print(eachPatient[eachVaccine][i])
+                print(eachVaccine, 
+                eachPatient[eachVaccine][i]["vaccination_name"], 
+                eachPatient[eachVaccine][i]["vacc_id"], 
+                eachPatient[eachVaccine][i]["date_given"], 
+                eachPatient[eachVaccine][i]["validity"])
+
+                post_functions.add_vaccination_record(eachVaccine, 
+                eachPatient[eachVaccine][i]["vaccination_name"], 
+                eachPatient[eachVaccine][i]["vacc_id"], 
+                eachPatient[eachVaccine][i]["date_given"], 
+                eachPatient[eachVaccine][i]["validity"]
+                )
+
 
 populate_vaccination_details()
