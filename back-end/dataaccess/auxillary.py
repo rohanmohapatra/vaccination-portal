090a7760bfbaae3e884c13401849391ee7a4d4df
Shivani Vogiral <32726676+vogiralshivani@users.noreply.github.com>
2019-11-17 22:13:39 +0530

    added "add_new_vaccination_type( )" function

--- back-end/dataaccess/auxillary.py
+++ back-end/dataaccess/auxillary.py
@@ -4,3 +4,24 @@ def get_types_vaccinations():
     with open("utils/types_vaccinations.json") as f:
         types_vaccinations = json.loads(f.read())
     return types_vaccinations
+
+
+
+def add_new_vaccination_type(vaccination_id, vaccination_name,validity):
+    types_vaccinations = dict()
+    with open("utils/types_vaccinations.json") as f:
+    	types_vaccinations = json.loads(f.read())
+    new_vaccination = {
+    	vaccination_id:
+    		{
+    			"vaccination_id": vaccination_id,
+    			"vaccination_name": vaccination_name,
+    			"validity": validity
+    		}
+    }
+    types_vaccinations.update(new_vaccination)
+    with open('utils/types_vaccinations','w') as f:
+    	json.dump(types_vaccinations,f)
+
+
+
