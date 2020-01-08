5a2d83526470107c16665359ac08f13a248674b0
rohanmohapatra <rohan.ron14@gmail.com>
2020-01-09 00:41:28 +0530

    Updates

---
+++ precache-manifest.487521da387b6510f9518190513413bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23c4310a1726952e298f313fc091627",
+    "url": "/vaccination-portal/index.html"
+  },
+  {
+    "revision": "0250b4001dfa3301ab37",
+    "url": "/vaccination-portal/static/css/2.fa4a79a6.chunk.css"
+  },
+  {
+    "revision": "58739f07ddd0d0a43ac5",
+    "url": "/vaccination-portal/static/css/main.879281ec.chunk.css"
+  },
+  {
+    "revision": "0250b4001dfa3301ab37",
+    "url": "/vaccination-portal/static/js/2.5ccab012.chunk.js"
+  },
+  {
+    "revision": "58739f07ddd0d0a43ac5",
+    "url": "/vaccination-portal/static/js/main.6b150802.chunk.js"
+  },
+  {
+    "revision": "5d454f1007dd288f2d73",
+    "url": "/vaccination-portal/static/js/runtime-main.e4434189.js"
+  }
+]);
